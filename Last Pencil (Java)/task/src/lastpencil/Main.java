b76160e8f45cd90e1fb14c07b52662bbbfd04b38
TomJozz <TomJozz@users.noreply.github.com>
2025-10-14 22:02:03 +0200

    Stage 3/5 complete

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -23,10 +23,40 @@ class PencilGame {
     }
 
     public void play() {
-        int pencils = promptForPencilCount();
+        int pencilsCount = promptForPencilCount();
         String firstPlayer = promptForFirstPlayer();
-        printPencils(pencils);
-        System.out.println(firstPlayer + " is going first!");
+        startGameLoop(pencilsCount, firstPlayer);
+    }
+
+    private void startGameLoop(int pencilsCount, String currentPlayer) {
+        while (pencilsCount >= MIN_PENCILS ) {
+            printPencils(pencilsCount);
+            System.out.println(currentPlayer + "'s turn:");
+            int numPencilsToUse = readValidPencilsToUseInput(pencilsCount);
+
+            pencilsCount -= numPencilsToUse;
+            currentPlayer = switchPlayer(currentPlayer);
+        }
+    }
+
+    private int readValidPencilsToUseInput(int pencilsCount) {
+        while (true) {
+            int numPencilsToUse = 0;
+            try {
+                numPencilsToUse = Integer.parseInt(scanner.nextLine());
+                if (numPencilsToUse > pencilsCount || numPencilsToUse < MIN_PENCILS) {
+                    if (pencilsCount == 1) {
+                        System.out.println("\uD83D\uDE44 There is only one left");
+                    } else {
+                        System.out.printf("You can't use less than %d or more than %d pencils!\n", MIN_PENCILS, pencilsCount);
+                    }
+                    continue;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number format! Please enter digits only.");
+            }
+            return numPencilsToUse;
+        }
     }
 
     private int promptForPencilCount() {
@@ -35,14 +65,13 @@ class PencilGame {
             try {
                 int pencils = Integer.parseInt(scanner.nextLine());
                 if (pencils < MIN_PENCILS || pencils > MAX_PENCILS) {
-                    throw new Exception("Pencils must be between " + MIN_PENCILS +" and " + MAX_PENCILS +"!");
+                    System.out.printf("Please enter a number between %d and %d.%n", MIN_PENCILS, MAX_PENCILS);
+                    continue;
                 }
                 return pencils;
 
             } catch (NumberFormatException e) {
-                System.out.println("Invalid number format!");
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+                System.out.println("Invalid number format! Please enter digits only.");
             }
         }
     }
@@ -60,12 +89,11 @@ class PencilGame {
         }
     }
 
-    private static void nextTurn() {
-        System.out.println("Your Turn!");
+    private String switchPlayer(String currentPlayer) {
+        return currentPlayer.equalsIgnoreCase("John") ? "Jack" : "John";
     }
 
     private static int generateRandomPencilCount () {
-        new Random().nextInt(3,4);
         return new Random().nextInt(MAX_PENCILS - MIN_PENCILS + 1) + MIN_PENCILS;
     }
 
